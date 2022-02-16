95888c5e2782cb6c340a5166346021523a481629
Arslan Abdullaev <ars352@yahoo.com>
2022-02-16 20:03:06 +0300

    Initial Commit

---
+++ ColorizedApp_SwiftBook/ColorizedApp_SwiftBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp_SwiftBookApp.swift
+//  ColorizedApp_SwiftBook
+//
+//  Created by Arslan Abdullaev on 16.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp_SwiftBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
